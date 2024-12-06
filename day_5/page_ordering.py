e8b625f2a87d997d81d0523168b274ead8d9fbc2
i-mehrotra <78836121+i-mehrotra@users.noreply.github.com>
2024-12-05 20:23:12 -0500

    first star

---
+++ day_5/page_ordering.py
@@ -0,0 +1,36 @@
+from ast import Dict
+from typing import List
+
+def page_orderer(path: str) -> int:
+    sum = 0
+    with open(path, "r") as file:
+        rules: Dict[int, List[int]] = {}
+
+        for line in file:
+            if "|" in line:
+                mid_index = line.index("|")
+                first = int(line[:mid_index])
+                second = int(line[mid_index+1:line.index("\n")])
+                if first not in rules.keys():
+                    rules[first] = [second]
+                else:
+                    rules.get(first).append(second)
+            elif "," in line:
+                sum += get_middle(line, rules)
+    return sum
+
+def get_middle(pages: str, rules: Dict) -> int:
+    pages = pages.strip("\n").split(",")
+    for i in range(len(pages)):
+        pages[i] = int(pages[i])
+        number = pages[i]
+        if number in rules.keys():
+            followers = rules.get(number)
+            for follower in followers:
+                if follower in pages:
+                    follower_index = pages.index(follower)
+                    if follower_index < i:
+                        return 0
+    return pages[(int(len(pages) / 2))]
+    
+print(page_orderer("day_5/input.txt"))
